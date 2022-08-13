9b39808545472d1e56940fe9c3bec04278b6b9bb
Tiber <danilo.ochoa.hidalgo@outlook.com>
2022-08-12 21:45:58 -0500

    Bookstore project with Testing

---
+++ README.md
@@ -0,0 +1,2 @@
+# expressjs-bookstore-testing
+Testing express application with Jest, Supertest and MongoClient
--- server.js
+++
@@ -1,6 +0,0 @@
-import database from './database.js'
-import makeApp from './app.js'
-
-const app = makeApp(database)
-
-app.listen(8080, () => console.log("listening on port 8080"))
